64902a97cf06c7967aa9bca04e6df345471dcbee
edgard <ed10131988@gmail.com>
2022-07-15 16:43:47 -0400

    embed messages

---
+++ messagesEmbeds.py
@@ -0,0 +1,7 @@
+#Rewards after combat
+#ARGS <XP> <Monedas>
+!alias rewards embed -title "Recompensa del coliseo! :stadium:" -desc “@everyone XP: %1% ⬆️ y Monedas: %2% 🪙 cada quien” -color #4AFF33
+
+#Sold items in shop
+#ARGS <Text> <Monedas>
+!alias vender embed -title "Casa de empeños" -desc %1% -footer "Ganancia: +%2% GP" -thumb https://i.imgur.com/5Y7t79u.png
