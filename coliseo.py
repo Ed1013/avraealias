c348bdab737669d7fbc2c63652f5ab3c7418bc84
Edgard <ed1013@bu.edu>
2023-04-14 16:07:26 -0400

    began implementing init automation !coliseo alias

--- coliseo.py
+++ coliseo.py
@@ -1,10 +1,17 @@
-!alias coliseo multiline
+!alias coliseo multiline embed
 <drac2>
 command = f'{ctx.prefix}i begin\n'
 coliseoSchema = load_json(get_uvar('planColiseo'))
+areasInfo = coliseoSchema.arena.areas
 
+string = ''
+for area in areasInfo:
+  string+=area.name
 
 </drac2>
+-title 'Nuevo plan coliseo'
+-desc {{dump_json(string)}}
+-color '#09963b'
 
 #-------------------------------------------
 
@@ -66,6 +73,7 @@ Template for  planning uvar planColiseo
   ]
 }
 """
+##json used by combatAreas metadata  [{"name": "test1", "members": []}, {"name": "test2", "members": []}, {"name": "test3", "members": ["KO1", "KO2", "KO3"]}]
 
 #teststring
 !planColiseo -arenaname Lorastir -arenaimg https://64.media.tumblr.com/78a9548e24a5d0dea1a6d149436fdee0/tumblr_p25s2gYY6o1r8ue9qo2_r1_400.png -area "Agua pantanosa" -area "Pasto lodos" -area "Arboles" -monster Xorn|3|https://www.dndbeyond.com/avatars/thumbnails/30836/551/1000/1000/638063939544338029.png -monster Kobold|4|https://www.dndbeyond.com/avatars/thumbnails/30832/207/1000/1000/638063832924455756.png
